14efce82da3d6630d3e1395d511a8f979a163167
Abdulkadir Hashi <dadirhashi544@gmail.com>
2026-03-14 17:14:05 +0100

    Add EF Core dependencies and PlatformToken model
    Added Entity Framework Core packages to enable database access and migrations. Introduced the PlatformToken model in the StarBord.Models namespace to represent platform tokens associated with businesses.

---
+++ Models/PlatformToken.cs
@@ -0,0 +1,13 @@
+﻿namespace StarBord.Models
+{
+    public class PlatformToken
+    {
+        public Guid Id { get; set; }
+        public Guid BusinessId { get; set; }
+        public string Platform { get; set; }
+        public string AccessToken { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public Business Business { get; set; }
+
+    }
+}
