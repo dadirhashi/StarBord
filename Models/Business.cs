aaa82636c56307aa55fc1865d958d8e333e96501
Abdulkadir Hashi <dadirhashi544@gmail.com>
2026-03-22 21:29:52 +0100

    i have changet busniess proberty from adress to address

--- Models/Business.cs
+++ Models/Business.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public string Name { get; set; }
-        public string Adress { get; set; }  
+        public string Address { get; set; }  
         public DateTime CreatedAt { get; set; }
         public User User { get; set; }
         public ICollection<Review> Reviews { get; set; }
