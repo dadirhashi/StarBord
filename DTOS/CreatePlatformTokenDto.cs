eee4b77e45fe9124e9e634737ae309c475dc0592
Abdulkadir Hashi <dadirhashi544@gmail.com>
2026-04-05 16:16:36 +0200

    Add CreatePlatformTokenDto for platform token data
    Introduced CreatePlatformTokenDto class in the StarBord.DTOS namespace with properties for BusinessId, Platform, AccessToken, and ExpiresAt to facilitate creation and transfer of platform token information.

---
+++ DTOS/CreatePlatformTokenDto.cs
@@ -0,0 +1,10 @@
+﻿namespace StarBord.DTOS
+{
+    public class CreatePlatformTokenDto
+    {
+        public Guid BusinessId { get; set; }
+        public string Platform { get; set; }
+        public string AccessToken { get; set; }
+        public DateTime ExpiresAt { get; set; }
+    }
+}
