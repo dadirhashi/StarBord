35d82e0e2daaf8ae3ee56ebd1ab15ccd088b903c
Abdulkadir Hashi <dadirhashi544@gmail.com>
2026-04-05 16:25:10 +0200

    Add GetPlatformTokenDto class for platform token data
    Introduced the GetPlatformTokenDto class in the StarBord.DTOS namespace. This DTO includes Id, BusinessId, Platform, and ExpiresAt properties to facilitate the transfer of platform token information within the application.

---
+++ DTOS/GetPlatformTokeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace StarBord.DTOS
+{
+    public class GetPlatformTokenDto
+    {
+        public Guid Id { get; set; }
+        public Guid BusinessId { get; set; }
+        public string Platform { get; set; }
+        public DateTime ExpiresAt { get; set; }
+    }
+}
