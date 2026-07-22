e38beb18da10963aaf1654ff0a9137089c72c10e
Abdulkadir Hashi <dadirhashi544@gmail.com>
2026-07-22 13:52:29 +0200

    Start converting Responses to the CQRS pattern. This command is the create operation for responses; the Trustpilot integration will be added to this same handler later

---
+++ Application/Responses/RespondToReviewCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using StarBord.Data;
+using StarBord.DTOS;
+
+namespace StarBord.Application.Responses
+{
+    public record RespondToReviewCommand (Guid ReviewId): IRequest<GetResponseDto>;
+    public class RespondToReviewCommandHandler :IRequestHandler<RespondToReviewCommand,GetResponseDto >
+    {
+        private readonly StarBordDbContext _context;
+        public RespondToReviewCommandHandler(StarBordDbContext context) => _context = context;
+
+        public async Task<GetBusinessDto> Handle(RespondToReviewCommand requst, CreateBusinessDto business)
+        {
+            var respond = await _context.FindAsync<RespondToReviewCommand(requst, business);>
+        }
+    
+    }
+}
